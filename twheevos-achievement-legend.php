26819c790121a78ad84a51b2f1286a930e6d6bfb
Ryan C. Gordon <icculus@icculus.org>
2021-09-06 02:50:45 -0400

    Even Veerender doesn't tweet _that_ much.  :)

--- twheevos-achievement-legend.php
+++ twheevos-achievement-legend.php
@@ -4,7 +4,7 @@
 function look_for_new_achievements__legend()
 {
     $twitter = prep_twitter();
-    $twitter->setGetfield('?screen_name=Veeren_Jubbal&count=100');
+    $twitter->setGetfield('?screen_name=Veeren_Jubbal&count=30');
     $json = $twitter->buildOAuth('https://api.twitter.com/1.1/statuses/user_timeline.json', 'GET')->performRequest();
     $response = json_decode($json, false, JSON_INVALID_UTF8_SUBSTITUTE);
     //print($json);
